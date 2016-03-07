c3d2ff58dbc4d7c78d9723c924355ea1ddfa9f4b
Cobalt Chang <cobaltchang@nexcom.com.tw>
2016-03-07 02:09:46 -0500

    1. Configure swagger
    	modified:   demo/demo/settings.py
    	modified:   demo/demo/urls.py

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^swagger/', include('rest_framework_swagger.urls')),
     url(r'^djoser/', include('djoser.urls')),
 ]
